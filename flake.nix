fcc0c2e5b4e4eba54bec42aaa25234d960d13220
Cainã Costa <me@cfcosta.com>
2025-09-28 18:25:38 -0300

    chore: fix nix build

--- flake.nix
+++ flake.nix
@@ -91,16 +91,17 @@
             src = ./.;
             cargoLock.lockFile = ./Cargo.lock;
             buildInputs = [
-              pkgs.pipewire
-              pkgs.libinput
-              pkgs.libudev-zero
-              pkgs.swayosd
-              pkgs.gst_all_1.gstreamer
+              pkgs.alsa-lib
+              pkgs.gst_all_1.gst-libav
+              pkgs.gst_all_1.gst-plugins-bad
               pkgs.gst_all_1.gst-plugins-base
               pkgs.gst_all_1.gst-plugins-good
-              pkgs.gst_all_1.gst-plugins-bad
               pkgs.gst_all_1.gst-plugins-ugly
-              pkgs.gst_all_1.gst-libav
+              pkgs.gst_all_1.gstreamer
+              pkgs.libinput
+              pkgs.libudev-zero
+              pkgs.pipewire
+              pkgs.swayosd
 
               whisper
             ];
